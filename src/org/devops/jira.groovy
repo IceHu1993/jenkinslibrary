3ddcf7a21359f671eed18bb592bb15b7d5f41806
Ice Hu <44312804+IceHu1993@users.noreply.github.com>
2020-05-20 14:01:03 +0800

    Update jira.groovy

--- src/org/devops/jira.groovy
+++ src/org/devops/jira.groovy
@@ -4,7 +4,7 @@ package org.devops
 def HttpReq(reqType,reqUrl,reqBody){
     def apiServer = "http://192.168.202.101:8080/rest/api/2"
    
-   result = httpRequest authentication: 'jira-software-users', //'jira-admin-user'
+   result = httpRequest authentication: 'jira-api-token', 
             httpMode: reqType, 
             contentType: "APPLICATION_JSON",
             consoleLogResponseBody: true,
